76409f5e94cfa497d151f29a348b106467c3fe07
angeldavid06 <mendozaangel.3243@gmail.com>
2021-07-15 17:38:17 -0500

    2021-07-15 045:38pm Inserción de proyectos

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ document.addEventListener('scroll', (evt) => {
         nav[0].classList.remove('bg-aux');
     }
 });
+
+if (document.getElementsByClassName('btn-menu-icon')) {
+    const menu_icon = document.getElementsByClassName('btn-menu-icon');
+    menu_icon[0].addEventListener('click', () => {
+        const nav = document.getElementsByClassName('nav');
+        const nav_items = document.getElementsByClassName('nav-items');
+        nav_items[0].classList.toggle('nav-items-show');
+    });
+}
