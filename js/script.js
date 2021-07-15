2780fd8f60e6d6450111b027660aa3ca7b992af5
angeldavid06 <mendozaangel.3243@gmail.com>
2021-07-15 18:18:31 -0500

    2021-07-15 06:17 pm Cambio en el archivo script js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-document.addEventListener('scroll', (evt) => {
+window.addEventListener('scroll', (evt) => {
     const nav = document.getElementsByClassName('nav');
+    const card = document.getElementsByClassName('card');
     if (window.scrollY > 10) {
         nav[0].classList.add('bg-aux');
     } else {
         nav[0].classList.remove('bg-aux');
+    }
+
+    for (let i = 0; i < card.length; i++) {
+        if (window.pageYOffset >= (card[i].offsetTop - 600)) {
+            card[i].classList.add('card-show');
+        } else {
+            card[i].classList.remove('card-show');
+        }
     }
 });
 
