6ea8d64fc9a3026a5370b7e8c8256599a1690367
angeldavid06 <mendozaangel.3243@gmail.com>
2021-07-16 17:40:02 -0500

    2021-07-16 05:40 pm Implementación de un carrusel y anexo de la guia de estilos

--- js/script.js
+++ js/script.js
@@ -52,3 +52,73 @@ if (document.getElementsByClassName('btn-menu-icon')) {
         nav_items[0].classList.toggle('nav-items-show');
     });
 }
+
+if (document.getElementsByClassName('close-modal')) {
+    const btn = document.getElementsByClassName('close-modal');
+    btn[0].addEventListener('click', () => {
+        const modal = document.getElementsByClassName('modal');
+        modal[0].classList.remove('modal-show');
+        vaciarModal();
+    });
+}
+
+const route_images_itt = [
+    'proyects/oferta_academica.PNG',
+    'proyects/oferta_academica_2.PNG',
+    'proyects/oferta_academica_3.PNG'
+];
+
+const route_images_vestidos = [
+    'proyects/vestidos.PNG',
+    'proyects/vestidos_2.PNG',
+    'proyects/vestidos_3.PNG',
+    'proyects/vestidos_4.PNG'
+];
+
+const route_images_estilos = [
+    'proyects/estilos.PNG',
+    'proyects/estilos_2.PNG',
+    'proyects/estilos_3.PNG',
+    'proyects/estilos_4.PNG',
+    'proyects/estilos_5.PNG'
+];
+
+const route_images_citapp = [
+    'proyects/citapp.PNG',
+    'proyects/citapp_2.PNG',
+    'proyects/citapp_3.PNG',
+    'proyects/citapp_4.PNG'
+];
+
+const route_images_pixki = [
+    'proyects/pixki.PNG',
+    'proyects/pixki_2.PNG',
+    'proyects/pixki_3.PNG',
+    'proyects/pixki_4.PNG'
+];
+
+const vaciarModal = () => {
+    const container = document.getElementById('container-images');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    auxiliar = 0;
+}
+
+document.addEventListener('click', (evt) => {
+    if (evt.target.matches('img')) {
+        const modal = document.getElementsByClassName('modal');
+        modal[0].classList.add('modal-show');
+        if (evt.target.dataset.img == 'oferta') {
+            render_images(route_images_itt);
+        } else if (evt.target.dataset.img == 'vestidos') {
+            render_images(route_images_vestidos);
+        } else if (evt.target.dataset.img == 'estilos') {
+            render_images(route_images_estilos);
+        } else if (evt.target.dataset.img == 'citapp') {
+            render_images(route_images_citapp);
+        } else if (evt.target.dataset.img == 'pixki') {
+            render_images(route_images_pixki);
+        }
+    }
+});
