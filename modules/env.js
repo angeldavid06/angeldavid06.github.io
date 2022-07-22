257f8a66fb998cb8ed345b7ebde3bf61f0b83719
angeldavid06 <mendozaangel.3243@gmail.com>
2022-07-22 14:03:16 -0500

    Peticiones FETCH API habilitadas

--- modules/env.js
+++ modules/env.js
@@ -1,21 +1,21 @@
 export const APP = {
     "URL_HOST" : "http://localhost/angeldavid06.github.io/",
     "D" : document,
-    "RES" : {}
+    "RESPONSE" : {}
 }
 
 export const fetchAPI = async (url) => {
     await fetch(url)
     .then((res => res.ok ? res.json() : Promise.reject()))
     .then(json => {
-        APP.RES = json;
+        APP.RESPONSE = json;
     })
     .catch(err => {
-        APP.RES = err;
+        APP.RESPONSE = err;
     })
     .finally(() => {
         console.log('Bien');
     })
 
-    return APP.RES;
+    return APP.RESPONSE;
 }
