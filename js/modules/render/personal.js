88c6441d7fe0ab6b152071ec92c130a0f6f92e91
angeldavid06 <mendozaangel.3243@gmail.com>
2023-06-24 21:03:23 -0600

    render header

--- js/modules/render/personal.js
+++ js/modules/render/personal.js
@@ -1,3 +1,16 @@
+import { UI } from "../ui.js";
+
 export const initPersonal = (data) => {
-    console.log(data);
+    const attributes = [
+        ['class', 'title main-title'],
+        ['id', 'name'],
+        ['data-name', 'true'],
+    ];
+
+    const container = UI.nElement('header');
+    const h1_name = UI.nElement('h1', attributes, data.name);
+    const h1_lastname = UI.nElement('h1', attributes, data.lastname);
+    const header = UI.join(container, [h1_name, h1_lastname]);
+
+    UI.print(header);
 }
--- js/modules/ui.js
+++ js/modules/ui.js
@@ -1,6 +1,29 @@
-const newElement = (element, attributes) => {
-    let el = null;
-    el = document.createElement(element.toLowerCase());
+export const UI = {
+    nElement : (element, attributes, text) => {
+        let el = null;
+        el = document.createElement(element.toLowerCase());
+    
+        if (attributes) {
+            attributes.forEach(attribute => {
+                el.setAttribute(attribute[0], attribute[1]);
+            });
+        }
 
-    return el;
+        if (text) {
+            el.innerHTML = text
+        }
+    
+        return el;
+    },
+    join : (container, elements) => {
+        console.log(elements);
+        elements.forEach(element => {
+            container.appendChild(element);
+        });
+
+        return container;
+    },
+    print : (code) => {
+        document.body.appendChild(code);
+    }
 }
